326c334a5f8d93b6157c72ae83b0bd6da7e1b0fe
ilya <ilyabelow.almost.surely@gmail.com>
2025-12-30 22:53:22 +0400

    Move to gnu-efi Makefiles
    Use only make infrastracture provided with gnu-efi
    Technically one is suppoed to edit apps/Makefile, but that's weird, and my approach works just fine

--- forward.c
+++ forward.c
@@ -2,11 +2,11 @@
 #include <efilib.h>
 
 #ifndef PART_UUID
-#define PART_UUID L"unknown"
+#define PART_UUID u"unknown"
 #endif
 
 #ifndef TARGET
-#define TARGET L"unknown"
+#define TARGET u"unknown"
 #endif
 
 
@@ -14,7 +14,8 @@ EFI_STATUS
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
   InitializeLib(ImageHandle, SystemTable);
-  Print(L"Forwarding to %s/%s\n", PART_UUID, TARGET);
+  Print(u"Forwarding to %s/%s\n", PART_UUID, TARGET);
+  return EFI_SUCCESS; // tmp
 
   UINTN handlesCount;
   EFI_HANDLE *handles;
@@ -26,11 +27,11 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     &handles
   );
   if (res != EFI_SUCCESS) {
-    Print(L"fail! %d\n", res);
+    Print(u"fail! %d\n", res);
     return EFI_ABORTED;
 
   }
-  Print(L"success! %d handles found\n", handlesCount);
+  Print(u"success! %d handles found\n", handlesCount);
 
   for (UINTN i = 0; i < handlesCount; i++) {
     EFI_DEVICE_PATH *path_protocol = DevicePathFromHandle(handles[i]);
@@ -38,14 +39,14 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
     EFI_DEVICE_PATH_TO_TEXT_PROTOCOL *text_protocol;
     uefi_call_wrapper(BS->LocateProtocol, 3, &gEfiDevicePathToTextProtocolGuid, NULL, (VOID**)&text_protocol);
     CHAR16* path = uefi_call_wrapper(text_protocol->ConvertDevicePathToText, 3, path_protocol, TRUE, TRUE);
-    Print(L"path: %s\n", path);
+    Print(u"path: %s\n", path);
     
-    Print(L"Now iteratively\n", path);
+    Print(u"Now iteratively\n", path);
     
     while (!IsDevicePathEnd(path_protocol)) {
       path = uefi_call_wrapper(text_protocol->ConvertDeviceNodeToText, 3, path_protocol, TRUE, TRUE);
-      Print(L"path: %s\n", path);
-      Print(L"%d: type %u subtype %u length %d\n", i, path_protocol->Type, path_protocol->SubType, DevicePathNodeLength(path_protocol));
+      Print(u"path: %s\n", path);
+      Print(u"%d: type %u subtype %u length %d\n", i, path_protocol->Type, path_protocol->SubType, DevicePathNodeLength(path_protocol));
 
       path_protocol = NextDevicePathNode(path_protocol);
     }
